2fa26fb4fdef477e2e6d36fe26b5580b5ecd2537
Daisuke Miyazaki <daisuke.miyazaki012@gmail.com>
2025-01-12 09:59:38 +0900

    feat: quit sass, and welcome tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    mode: 'jit',
+    content: [
+      './_drafts/**/*.html',
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_posts/*.md',
+      './*.md',
+      './*.html',
+    ],
+    theme: {
+      extend: {
+        typography: (theme) => ({
+          DEFAULT: {
+            css: {
+              h1: {
+                // color: theme('colors.red.500'),
+                // fontWeight: 'bold',
+              },
+            },
+          },
+        }),
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
