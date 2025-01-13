dda48609276fbb43517a3a3f62dcabba3e400ff3
Daisuke Miyazaki <daisuke.miyazaki012@gmail.com>
2025-01-13 21:30:23 +0900

    feat: implemented photoscroll react integration to site

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
     plugins: [
       require('tailwindcss'),
       require('autoprefixer'),
+    require("tailwindcss/nesting"),
+    require("tailwindcss"),
       ...(process.env.JEKYLL_ENV == 'production'
         ? [require('cssnano')({ preset: 'default' })]
         : [])
