65a3df83e4e58b6c48299a67a3b68382a1de2cab
Daisuke Miyazaki <daisuke.miyazaki012@gmail.com>
2025-01-20 22:35:09 +0900

    feat: wrote a draft on indentation ver1

---
+++ src/indentations/indentations.ts
@@ -0,0 +1,115 @@
+export default function data() {
+  return [
+    {
+      id: 1,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description: "例の凸凹",
+    },
+    {
+      id: 1,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description:
+        "壁いっぱいに空間を使えるようなデザインにした。天井までのスペースもかなりあるので、高さを有効に活用することで収納力を増やすことができる。",
+    },
+    {
+      id: 2,
+      src: "/assets/images/indentation/IMG_1895.jpg",
+      description:
+        "まずこの作品がある前の状態の写真を貼っておく。椅子しかなく、リラックスするにも空間が上手く使えていなかった。",
+    },
+    {
+      id: 3,
+      src: "/assets/images/indentation/IMG_1758.jpg",
+      description:
+        "今回の工夫したポイントの縁の下の力持ちはこちら。少し値段が張ったが、骨組みそのものを2by2でやっていこうとすると単純なビスで留めていくのは干渉する危険があった",
+    },
+    {
+      id: 4,
+      src: "/assets/images/indentation/IMG_1646.jpg",
+      description:
+        "Kregを打ち込むための先端器具を取り付け、一つ一つの接合部分端に2つずつ穴を開けていく。",
+    },
+    {
+      id: 5,
+      src: "/assets/images/indentation/IMG_1747.jpg",
+      description:
+        "両端にあてはめると4つ穴となる。これを接合していく部分にひたすら開けていく作業。慣れてしまえばペースができるし勝手はわかるのだが、数が多いのでこれだけで一日かかってしまった。...",
+    },
+    {
+      id: 6,
+      src: "/assets/images/indentation/IMG_1958.jpg",
+      description:
+        "大量の木材。骨組みだけだが、これにずっと穴を開けていくだけでかなり時間ばかり過ぎていく。",
+    },
+    {
+      id: 7,
+      src: "/assets/images/indentation/IMG_1911.jpg",
+      description:
+        "切削部分から、ばかにならない量の木屑も発生する。机でうけつつもバケツを横に置いて廃棄していったが、それでも床は木屑だらけになった。",
+    },
+    {
+      id: 8,
+      src: "/assets/images/indentation/IMG_1946.jpg",
+      description:
+        "こちらにおそらくほぼ丸一日くらいかかっている。Kregで同じように購入したクランプを使用し、昇降式のテーブルに固定しながら 2 by 2の両辺など必要なところに穴をあけていく。",
+    },
+    {
+      id: 9,
+      src: "/assets/images/indentation/IMG_1675.jpg",
+      description:
+        "それぞれの木片に穴が空ができたら、そこから基盤となるパーツを作っていく。こちらはソファ下に入れる3点。上面でソファの荷重を受ける。中心部分は2by4で作成したのは、面の面積が大きいから。2by2では心許なかった。",
+    },
+    {
+      id: 10,
+      src: "/assets/images/indentation/IMG_1681.jpg",
+      description:
+        "壁となる部分を作っていく。単体の部品から最小単位となるように設計していくことで、後々解体するときに再利用が可能となる。これで、縦の部品が完成となる。ここから横材留めていく。",
+    },
+    {
+      id: 11,
+      src: "/assets/images/indentation/IMG_1986.jpg",
+      description:
+        "それぞれの素材を横材でとめながら設計書通りにとめていく。一気に骨格ができてきてこの辺で気分が上がってくる。",
+    },
+    {
+      id: 12,
+      src: "/assets/images/indentation/IMG_1884.jpg",
+      description:
+        "骨組みができたら全てを固定する前に、一度仕様で定めたソファが乗るか、そして格納したい黒テーブルが入り切るのかを確認していく",
+    },
+    {
+      id: 13,
+      src: "/assets/images/indentation/IMG_1876.jpg",
+      description: "ふっと一息。",
+    },
+    {
+      id: 14,
+      src: "/assets/images/indentation/IMG_1743.jpg",
+      description:
+        "組み立ての途中で設計した左上の棚には中間をあけた方が見た目がよく、また高さのあるものも置くことができると判断し、そのまま四角となるようにした。",
+    },
+    {
+      id: 15,
+      src: "/assets/images/indentation/IMG_1963.jpg",
+      description:
+        "思い切り手ブレしている。。が、もう一つのソファもここで開封する。",
+    },
+    {
+      id: 16,
+      src: "/assets/images/indentation/IMG_1857.jpg",
+      description:
+        "木組みが終わって、とりあえず然る場所に置き戻してソファを置いて確認する。ソファ下部の木組に少し遊びがあるので、木組とソファ基礎を固定するような機構が必要であるとここで気づいた。",
+    },
+    {
+      id: 17,
+      src: "/assets/images/indentation/IMG_1857.jpg",
+      description:
+        "ここまでで、一度作業終了。全体の形が確認できて大満足。また、ソファをくみ横に置いてみると、大きなソファとなって座れて妻も大満足であった。収納機能や、照明などやることはまだまだある。",
+    },
+    {
+      id: 18,
+      src: "/assets/images/indentation/IMG_1857.jpg",
+      description: "ここからver2に進む。",
+    },
+  ];
+}
