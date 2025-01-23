e59b9c4caa2b7f4070e6cddb653651f5e72457c0
Daisuke Miyazaki <daisuke.miyazaki012@gmail.com>
2025-01-23 20:59:11 +0900

    feat: tentatively wrote up v2

---
+++ src/indentations/v2/indentations.ts
@@ -0,0 +1,115 @@
+export default function data() {
+  return [
+    {
+      id: 1,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description: "例の凸凹",
+    },
+    {
+      id: 1,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description:
+        "前回のver1では骨組みを作り、最低限のソファを用意。しかし要件である十分に必要なスペースを確保するには置き場所や収納を作る必要があった。",
+    },
+    {
+      id: 2,
+      src: "/assets/images/indentation/IMG_1683.jpg",
+      description:
+        "天板を作る。ただし構造上天板そのものが重くなってしまってはいけないのでなるべく軽い素材を使って作る。",
+    },
+    {
+      id: 4,
+      src: "/assets/images/indentation/IMG_1683.jpg",
+      description:
+        "材料は軽くて安価ながら丈夫な桐を選んだ。厚みがあるわけではないのでそのまま上に置いて固定しても割れてしまうので、土台となる天板の骨組みを作ってから桐天板をひいていく。横の長さがぴったりのものもなかったで、複数を横に並べてつくっていく。",
+    },
+    {
+      id: 5,
+      src: "/assets/images/indentation/IMG_1676.jpg",
+      description:
+        "構造上、隅の部分は切り取らないといけないのであまった 2 by 2の端材をもとに線を引きノコギリで切り落とす。一度でうまくハマるわけでもないので、試してはやすりがけを繰り返す。...",
+    },
+    {
+      id: 6,
+      src: "/assets/images/indentation/IMG_1728.jpg",
+      description:
+        "天板がうまくはまったら、木工用のボンドと瞬間接着剤を被らないようにぬってからクランプで固着する。全部で10枚弱くらい貼っているのだが、基本は同じ作業の繰り返しである。",
+    },
+    {
+      id: 7,
+      src: "/assets/images/indentation/IMG_1695.jpg",
+      description: "完成した様子はこちら",
+    },
+    {
+      id: 8,
+      src: "/assets/images/indentation/IMG_1730.jpg",
+      description:
+        "ソファから見た眺め。中棚の凸凹部分をあえて宙にあげる構造にしたので、柱がないためこのテーブルの使い勝手が良い。",
+    },
+    {
+      id: 9,
+      src: "/assets/images/indentation/IMG_1933.jpg",
+      description:
+        "本や雑貨など、実際にこの場所におきたいものも天板の上におきながら負荷テストなどを行っていく。",
+    },
+    {
+      id: 10,
+      src: "/assets/images/indentation/IMG_3319.jpg",
+      description:
+        "ここまで終わったら、次は引き出しに取り掛かる。収納可能なエリアはいか以下の3つ。",
+    },
+    {
+      id: 11,
+      src: "/assets/images/indentation/IMG_3319.jpg",
+      description:
+        "イメージをつけてからソファしたの引き出しを作る。左のテーブル下は扉部分は、この次の作業で行う。",
+    },
+    {
+      id: 12,
+      src: "/assets/images/indentation/indenteiton drawer.png",
+      description:
+        "構造そのものは単純だが、例の凸凹 ver1でのこった 2 by 2材を一部使うことで材料費を抑えた。顔となる前面は床から2mmくらい浮かすところで固定する。コーナンから買ってきた 2 by 4をメインで作ったのだが木材そのものが曲がっていたが、運用上横方向に力が働くこともないので看過した。",
+    },
+    {
+      id: 13,
+      src: "/assets/images/indentation/indenteiton drawer.png",
+      description:
+        "複雑なことはなく、とりあえず顔以外の枠を作っていくのみ。横幅は収納と1センチ弱くらい差分でとり底板として3枚使う。その上に、枠としてkregを底板に対して垂直方向に取り付けていく。",
+    },
+    {
+      id: 14,
+      src: "/assets/images/indentation/IMG_1969.jpg",
+      description:
+        "実際いろいろ入れてみる、奥行きがあるのでかなりの収納力がある。",
+    },
+    {
+      id: 15,
+      src: "/assets/images/indentation/IMG_1752.jpg",
+      description:
+        "また、下部分はキャスターをつけている。最後に、顔部分を浮かしつつ、取り付ける。",
+    },
+    {
+      id: 16,
+      src: "/assets/images/indentation/IMG_1906.jpg",
+      description:
+        "スッキリ収納されたときには安心感も覚えた。ただこのままだとサイズがぴったりすぎて取り出す取手がないとハマってしまう。顔の外にでるように取手をつけることもできたが、ソファに座っていると足が当たるところであることから上方向からの力が加わってしまう恐れがあるため不向きと判断。代わりに顔から内側方向に取手があるとよいので、大きすぎない穴をあけて取手とした。",
+    },
+    {
+      id: 17,
+      src: "/assets/images/indentation/IMG_1677.jpg",
+      description:
+        "この作業が終わったら、ニスを塗っていて仕上げに。ニスが乾いたら完成！椅子そのものは80cm四方、高さも20cmくらいはあるのでかなりの容量の物を収納できる。",
+    },
+    {
+      id: 18,
+      src: "/assets/images/indentation/IMG_3664.jpg",
+      description:
+        "完成品はこちら。一貫した見栄えがしっくりときた。ここまででガッツリ収納量が確保され、かなり終わりが見えてきた。",
+    },
+    {
+      id: 18,
+      src: "/assets/images/indentation/IMG_3664.jpg",
+      description: "ver3へ進む",
+    },
+  ];
+}
