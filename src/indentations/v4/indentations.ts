a19a8abf8476dfd2a2c73c7934da0e14b9402176
DaisukeMiyazaki <DaisukeMiyazaki@users.noreply.github.com>
2025-02-10 23:50:30 +0000

    deploy: c92ebd6164b9b368bf7bc906e978848b77a77055

---
+++ src/indentations/v4/indentations.ts
@@ -0,0 +1,176 @@
+export default function data() {
+  return [
+    {
+      id: 1,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description: "例の凸凹 ver4",
+    },
+    {
+      id: 2,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description:
+        "この部屋の間取りから、入り込んだ部分がはみ出していた。デットスペースとなっていたのでこちらを有効活用すること。ただし、壁に沿って空調調整のための口がついているのでこちらは塞がないように設計に注意する必要がある。",
+    },
+    {
+      id: 3,
+      src: "/assets/images/indentation/IMG_4769.jpeg",
+      description:
+        "天板の手前に顔がなかったので中に入っている季節ものの収納がさらされていたので、こちらを隠せるような顔を作成する。",
+    },
+    {
+      id: 4,
+      src: "/assets/images/indentation/IMG_4021.jpeg",
+      description:
+        "まずは左につくる特注の棚から。本体と比較して、小さな例の凸凹と名付けてみた。全体とのバランスをとって考えると棚として中身が見えるというより、普段からはあまり使わないものを収納しつつも取り出しやすいように可動式にする設計、意匠が良いと思われる。",
+    },
+    {
+      id: 6,
+      src: "/assets/images/indentation/IMG_4022.jpeg",
+      description: "接着にはkregを使うために、穴を開けていく",
+    },
+    {
+      id: 7,
+      src: "/assets/images/indentation/IMG_4025.jpeg",
+      description: "接着にはkregを使うために、穴を開けていく",
+    },
+    {
+      id: 7,
+      src: "/assets/images/indentation/IMG_4025.jpeg",
+      description:
+        "そしてビス穴を開けて接着！....とおもったが、穴部分の木材が端すぎるため木材が割れてしまった部分もあった。これは大きな失敗。",
+    },
+    {
+      id: 8,
+      src: "/assets/images/indentation/IMG_4028.jpeg",
+      description:
+        "仕方なく、全面的にボンドによる貼り付けで対応していく。強度がやや心配ではあったが、ビス穴では木材がボロボロになり、接着すらしないことが確認できたのでこれに賭けるしかなかった。設計のミスとも言える。",
+    },
+    {
+      id: 9,
+      src: "/assets/images/indentation/IMG_4033.jpeg",
+      description:
+        "クランプでしっかり留めていく。一個一個面倒くさいがやっていくしかない。",
+    },
+    {
+      id: 10,
+      src: "/assets/images/indentation/IMG_4034.jpeg",
+      description:
+        "クランプでしっかり留めていく。一個一個面倒くさいがやっていくしかない。",
+    },
+    {
+      id: 11,
+      src: "/assets/images/indentation/IMG_4037.jpeg",
+      description: "換気扇上のスペース部分のここも接着",
+    },
+    {
+      id: 12,
+      src: "/assets/images/indentation/IMG_4032.jpeg",
+      description: "また、裏面にはキャスターもつけていく。",
+    },
+    {
+      id: 13,
+      src: "/assets/images/indentation/IMG_4041.jpeg",
+      description: "組み上がったら、実際にニスを塗っていく。",
+    },
+    {
+      id: 14,
+      src: "/assets/images/indentation/IMG_4043.jpeg",
+      description: "様になってきている。",
+    },
+    {
+      id: 15,
+      src: "/assets/images/indentation/IMG_4049.jpeg",
+      description:
+        "完成したら、実際に本などを置いてみて耐久力をはかる。問題はなさそうである。",
+    },
+    {
+      id: 16,
+      src: "/assets/images/indentation/IMG_3322 1.jpg",
+      description:
+        "なお、この小さな例の凸凹のハンドルになる左上部分は、本体の命名元にもなっている棚の凸凹部分を意識して作ってみた。",
+    },
+    {
+      id: 17,
+      src: "/assets/images/indentation/IMG_4046.jpeg",
+      description: "設置した感じ。柱周辺にピタッとハマった。",
+    },
+    {
+      id: 18,
+      src: "/assets/images/indentation/IMG_4046.jpeg",
+      description:
+        "実際テストをしてみて、強度も問題ないことがわかった。ボンドが優秀だからかもしれない。リンクも貼っておく。",
+      link: "?next=https://amzn.asia/d/0aUhySwa",
+    },
+    {
+      id: 19,
+      src: "/assets/images/indentation/IMG_4046 highlighted.jpeg",
+      description:
+        "最後に、倉庫にあたる部分の扉作り。大きさがあってこの一枚板を再度発注してしまうと費用も嵩張ってしまうので、すでに購入済みの木材をボンドで簡単に圧着しつつ作成することに。",
+    },
+    {
+      id: 20,
+      src: "/assets/images/indentation/IMG_4051.jpeg",
+      description:
+        "ただ、これに対応する大きさのクランプも持っていないので、木片をつかってそれぞれを方向から力が逃げないように固定/固着していく。",
+    },
+    {
+      id: 21,
+      src: "/assets/images/indentation/IMG_4054.jpeg",
+      description: "両方をガッチリと止める",
+    },
+    {
+      id: 22,
+      src: "/assets/images/indentation/IMG_4055.jpeg",
+      description: "両方をガッチリと止める",
+    },
+    {
+      id: 23,
+      src: "/assets/images/indentation/IMG_2510.jpeg",
+      description:
+        "完全に固まったら、ニスを塗って乾かしていく。こちらでほとんど完成！",
+    },
+    {
+      id: 24,
+      src: "/assets/images/indentation/IMG_4777.jpeg",
+      description:
+        "冬用のヒーターや作業の時のみで使うため、こちらを開け閉めする頻度はほとんどない。そのため蝶番などを使った作りではなく、単純に受け口をつけるだけとした。頭の部分には、キャンディー用の木材を重ねた簡単な留め具を作成し、ビス留めをして回転するようにした。",
+    },
+    {
+      id: 25,
+      src: "/assets/images/indentation/IMG_4775.jpeg",
+      description: "ドアの後ろには木片を接着し扉が止まるように",
+    },
+    {
+      id: 26,
+      src: "/assets/images/indentation/IMG_4776.jpeg",
+      description: "左右に取り付ける",
+    },
+    {
+      id: 27,
+      src: "/assets/images/indentation/IMG_4774.jpeg",
+      description: "扉と相まって見るとピッタリ。",
+    },
+    {
+      id: 28,
+      src: "/assets/images/indentation/IMG_2313.jpeg",
+      description: "出来栄えはこの通り。",
+    },
+    {
+      id: 29,
+      src: "/assets/images/indentation/IMG_4779.jpeg",
+      description: "完成したした写真はこちら。",
+    },
+    {
+      id: 30,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description:
+        "これをもってこのプロジェクトを終了とする。例の凸凹が加わったことで、この部屋もAtelierとして十分に使える空間に変身した。",
+    },
+    {
+      id: 31,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description:
+        "振り返ってみて、時間はかかるが手間と強度という観点から、ダボ継ぎやネジを使わない木組みをやってみるのは、さらなるチャレンジになると思った。反面、ドリルを使ってガツガツ穴を開けていられるkreg商品は、それ専用の工具を買っているので値段は安く済んだわけではないが、強度上も見た目上も十分なものに仕上がったという印象。後悔はないし、何も考えないでビス留めして行くのはメンテナンス性にもかけたであろうという印象。ここまで大きな家具を作ったことはなく初めての挑戦であったが、部屋の間取りを十分に活用することができた。",
+    },
+  ];
+}
