ffb7fabcd037eafce8d545c644dcc43179b57c1d
DaisukeMiyazaki <DaisukeMiyazaki@users.noreply.github.com>
2025-07-09 09:21:46 +0000

    deploy: 567560d9729f7e43ac2f8ca6ff20ee9aa4e08aa0

---
+++ src/indentations/en/indentations.ts
@@ -0,0 +1,117 @@
+export default function data() {
+  return [
+    {
+      id: 0,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description:
+        "A 6-tatami mat room. To use this as a workshop for making miscellaneous goods, I wanted it to be not just for storing materials, but also as a display space, a workspace, and a relaxing area. I've written down the process of a large-scale challenge to build furniture that fulfills these desires.",
+    },
+    {
+      id: 1,
+      src: "/assets/images/indentation/IMG_4780.jpeg",
+      description:
+        "The design utilizes the entire wall space. There's also plenty of space up to the ceiling, so by making effective use of the height, storage capacity can be increased.",
+    },
+    {
+      id: 2,
+      src: "/assets/images/indentation/IMG_1895.jpg",
+      description:
+        "First, here's a photo of the state before this project. There was only a chair, and the space wasn't being used effectively for relaxation.",
+    },
+    {
+      id: 3,
+      src: "/assets/images/indentation/IMG_1758.jpg",
+      description:
+        "Here's the unsung hero of this project's ingenuity. You can't see the nail marks, and since they're driven from the inside, it provides strength. It was a bit pricey, but trying to build the frame with 2x2s using simple screws would have risked interference.",
+    },
+    {
+      id: 4,
+      src: "/assets/images/indentation/IMG_1646.jpg",
+      description:
+        "Attach the tip for driving the Kreg screws and drill two holes at the end of each joint. I had to buy the jig.",
+    },
+    {
+      id: 5,
+      src: "/assets/images/indentation/IMG_1747.jpg",
+      description:
+        "When applied to both ends, it makes four holes. The task is to endlessly drill these into the parts to be joined.",
+    },
+    {
+      id: 6,
+      src: "/assets/images/indentation/IMG_1958.jpg",
+      description:
+        "A large amount of lumber. Once you get used to it, you find a rhythm and get the hang of it, but there were so many pieces that this alone took a whole day...",
+    },
+    {
+      id: 7,
+      src: "/assets/images/indentation/IMG_1911.jpg",
+      description:
+        "A non-trivial amount of wood chips are also generated from the cutting. I tried to catch them on a desk and had a bucket on the side for disposal, but the floor still ended up covered in wood chips.",
+    },
+    {
+      id: 8,
+      src: "/assets/images/indentation/IMG_1946.jpg",
+      description:
+        "Halfway point, this probably took about a full day. Using a clamp to hold things while drilling makes it somewhat easier.",
+    },
+    {
+      id: 9,
+      src: "/assets/images/indentation/IMG_1675.jpg",
+      description:
+        "Once all the wooden pieces have holes, I start making the base parts. These are the three pieces that go under the sofa. The top surface bears the load of the sofa. The central part was made with a 2x4 because the surface area is large; a 2x2 would have been unreliable.",
+    },
+    {
+      id: 10,
+      src: "/assets/images/indentation/IMG_1681.jpg",
+      description:
+        "Building the wall sections. By designing them from single parts to be the smallest possible units, they can be reused later when disassembling. With this, the vertical components are complete.",
+    },
+    {
+      id: 11,
+      src: "/assets/images/indentation/IMG_1986.jpg",
+      description:
+        "Fastening each material with horizontal beams according to the design plan. The frame comes together all at once, and at this point, my spirits are lifting.",
+    },
+    {
+      id: 12,
+      src: "/assets/images/indentation/IMG_1884.jpg",
+      description:
+        "Once the frame is built, before fixing everything in place, I check if the specified sofa fits on it and if the black table I want to store fits inside.",
+    },
+    {
+      id: 13,
+      src: "/assets/images/indentation/IMG_1876.jpg",
+      description: "A short break.",
+    },
+    {
+      id: 14,
+      src: "/assets/images/indentation/IMG_1743.jpg",
+      description:
+        "The top-left shelf, designed during assembly, was changed from the original plan. Removing the middle partition looked better, and I decided it would also allow for taller items to be placed there.",
+    },
+    {
+      id: 15,
+      src: "/assets/images/indentation/IMG_1963.jpg",
+      description:
+        "Completely blurry photo... I'm unboxing the other sofa here too.",
+    },
+    {
+      id: 16,
+      src: "/assets/images/indentation/IMG_1857.jpg",
+      description:
+        "After finishing the woodwork, I put it back in place for now, placed the sofa on it, and checked. There's a little bit of play in the wooden frame under the sofa, so I realize a mechanism to fix the frame to the sofa base is necessary.",
+    },
+    {
+      id: 17,
+      src: "/assets/images/indentation/IMG_1857.jpg",
+      description:
+        "This is it for now. I'm very satisfied with how the overall shape turned out. Also, when I assembled the sofa and placed it, it became a large sofa to sit on, and my wife was very happy. There are still many things to do, like storage functions and lighting.",
+    },
+    {
+      id: 18,
+      src: "/assets/images/indentation/IMG_1857.jpg",
+      description: "Proceeding to ver2 from here.",
+      link: "/en/2025/01/20/The-indentation-Project-ver2/",
+    },
+  ];
+}
