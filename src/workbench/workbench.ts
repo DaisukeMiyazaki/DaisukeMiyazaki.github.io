a56c3cfa3aad0582cb5c237bd222f8237abf037e
DaisukeMiyazaki <DaisukeMiyazaki@users.noreply.github.com>
2025-07-14 07:30:36 +0000

    deploy: 30e758cfa57b5a527bb94067c94803ff3c2e745f

--- src/workbench/workbench.ts
+++ src/workbench/workbench.ts
@@ -2,119 +2,119 @@ export default function data() {
   return [
     {
       id: 1,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/front.jpeg",
+      src: "/assets/images/workbench/front.jpeg",
       description:
         "こんにちは、皆さん！先月頭、アート愛好者の妻のために、どこにも売っていない特別なワークベンチを作る挑戦をしてみました。妻がアートを楽しんでいる様子を見るのは素晴らしいこと。でも5畳もないお部屋で散らかり具合...収納が足りない！そして、どうしても座って書いていると姿勢が...そこで立ったままアートを楽しむことができるワークベンチがあれば、こんな悩みも解消されるんじゃないかと。そう思って作りはじめてみました。それでは、DIYの開始です！",
     },
     {
       id: 2,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/test0.jpeg",
+      src: "/assets/images/workbench/test0.jpeg",
       description: "座っている状態だとどうしても目線が下がってしまいます。",
     },
     {
       id: 3,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/test1.jpeg",
+      src: "/assets/images/workbench/test1.jpeg",
       description: "この高さだと、まだ顔が下を向いていて首が疲れてしまいそう。",
     },
     {
       id: 4,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/test3.jpeg",
+      src: "/assets/images/workbench/test3.jpeg",
       description:
         "この高さなら、全体的にまっすぐになっていて良さそう。立った時と比較しても、大体同じくらい中心線になっているのがわかります。こちらで高さは110センチくらいが良さそうだとわかりました。",
     },
     {
       id: 5,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/cad.jpeg",
+      src: "/assets/images/workbench/cad.jpeg",
       description:
         "次に3Dモデリングで設計します。全体のバランスや重心の計算、イメージのすり合わせなどを妻のフィードバックを得ながら作っていきます。こだわった点は...",
     },
     {
       id: 6,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/inProgress.jpeg",
+      src: "/assets/images/workbench/inProgress.jpeg",
       description:
         "木材が届いたら、あとは作るのみ。イメージ通りに作っていきます。こちら途中経過...",
     },
     {
       id: 7,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/miniTruck.jpeg",
+      src: "/assets/images/workbench/miniTruck.jpeg",
       description:
         "扉は寸法もモデルで見ていた通りにならなかったので後付けにしました。買い出しに再度ホームセンターのコーナンへ、暑いので自転車でむかったものの巨大すぎるドアをどうも運べず。再度配送料を払うのもバカバカしい。。。",
     },
     {
       id: 8,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/miniTruck2.jpeg",
+      src: "/assets/images/workbench/miniTruck2.jpeg",
       description:
         "そんなときなんと、コーナンでは軽トラを無料で借りれることが判明。ハンドルつき、普通免許で運転できることも初めて知りました。おかげ様でスムーズに運ぶことができました。",
     },
     {
       id: 9,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/hotMan.jpeg",
+      src: "/assets/images/workbench/hotMan.jpeg",
       description:
         "仕上げにはニス塗りもしっかりと。あっつい部屋でもしっかりと換気をしつつ。真夏のクーラーの効かない部屋で二重マスク。",
     },
     {
       id: 10,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/legs.jpeg",
+      src: "/assets/images/workbench/legs.jpeg",
       description:
         "仕上げには転倒対策。大きなものなのでこちらも必須です。モデルでは足を強固にする予定だったけれど加工が手間で面倒くさい。。。色々考えてみた結果、ホームセンターで見つけたこいつ一枚で解決。",
     },
     {
       id: 11,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/seita.jpeg",
+      src: "/assets/images/workbench/seita.jpeg",
       description: "また、横揺れもしたので背板をつけることで解消しました。",
     },
     {
       id: 12,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/front.jpeg",
+      src: "/assets/images/workbench/front.jpeg",
       description: "ついに完成！",
     },
     {
       id: 13,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/front2.jpeg",
+      src: "/assets/images/workbench/front2.jpeg",
       description: "完成したワークベンチ",
     },
     {
       id: 14,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/front3.jpeg",
+      src: "/assets/images/workbench/front3.jpeg",
       description: "完成したワークベンチ",
     },
     {
       id: 15,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/tableWidth.jpeg",
+      src: "/assets/images/workbench/tableWidth.jpeg",
       description: "折りたたみ式の天板も180cm * 50cmと十分な広さがあります。",
     },
     {
       id: 16,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/faceToCamera.jpeg",
+      src: "/assets/images/workbench/faceToCamera.jpeg",
       description: "折りたたみ式の天板も180cm * 50cmと十分な広さがあります。",
     },
     {
       id: 17,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/positionCheck.jpeg",
+      src: "/assets/images/workbench/positionCheck.jpeg",
       description:
         "立った時の姿勢も、測ったときの姿勢とほぼ一致しています。これで快適にアートに取り組めるはず！",
     },
     {
       id: 18,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/foldedTableTop.jpeg",
+      src: "/assets/images/workbench/foldedTableTop.jpeg",
       description:
         "天板が扉の役割も兼用し、コードなども隠して見た目もすっきりするように。",
     },
     {
       id: 19,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/mount.jpeg",
+      src: "/assets/images/workbench/mount.jpeg",
       description:
         "天板が扉の役割も兼用し、コードなども隠して見た目もすっきりするように。",
     },
     {
       id: 20,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/codes.jpeg",
+      src: "/assets/images/workbench/codes.jpeg",
       description:
         "天板が扉の役割も兼用し、コードなども隠して見た目もすっきりするように。",
     },
     {
       id: 21,
-      src: "https://storage.googleapis.com/daisukemiyazaki_website/builds/front.jpeg",
+      src: "/assets/images/workbench/front.jpeg",
       description:
         "いかがでしょうか？皆さんも、身近なものやサービスを利用しながら、特別なものをDIYで作って贈ってみてはみてはいかがでしょうか？",
     },
